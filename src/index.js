123d8a2ebdeab1218e7bcc8007135bd8807728ea
Gautier Borot <g.borot@gmail.com>
2020-02-20 09:46:41 +0100

    setup to connect client server with socket.io

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+// socket need the raw http server (express does it in the background and we don't have access)
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("New WebSocket connection");
+});
 
 server.listen(port, () => {
   console.log("Server is up on port " + port);
