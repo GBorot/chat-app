1b86eca9bbb077d276b1041fe1464ba4b787305d
Gautier Borot <g.borot@gmail.com>
2020-02-20 10:30:46 +0100

    added a form, added event listenner on that form and emit to server, listen to that message and printed it (my version so next it's correction)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,25 @@
 const socket = io();
 
-socket.on("countUpdated", count => {
-  console.log("The count has been updated", count);
+// socket.on("countUpdated", count => {
+//   console.log("The count has been updated", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//   console.log("clicked");
+//   socket.emit("increment");
+// });
+
+socket.on("message", message => {
+  console.log(message);
 });
 
-document.querySelector("#increment").addEventListener("click", () => {
-  console.log("clicked");
-  socket.emit("increment");
+const chatForm = document.querySelector("form");
+const text = document.querySelector("input");
+
+chatForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const message = text.value;
+
+  socket.emit("sendMessage", message);
 });
--- src/index.js
+++ src/index.js
@@ -13,21 +13,27 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+// let count = 0;
 
 // server (emit) -> client (receive) -> countUpdated
 // client (emit) -> server (receive) -> increment
 io.on("connection", socket => {
   console.log("New WebSocket connection");
 
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    // the first one emits but other users can't see the update until refresh
-    // socket.emit("countUpdated", count);
-    // this one emits to every single connection
-    io.emit("countUpdated", count);
+  socket.emit("message", "Welcome!");
+
+  socket.on("sendMessage", message => {
+    io.emit("message", message);
   });
+
+  // socket.emit("countUpdated", count);
+  // socket.on("increment", () => {
+  //   count++;
+  //   // the first one emits but other users can't see the update until refresh
+  //   // socket.emit("countUpdated", count);
+  //   // this one emits to every single connection
+  //   io.emit("countUpdated", count);
+  // });
 });
 
 server.listen(port, () => {
