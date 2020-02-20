8a5290c11c156468465f3641b5080b783154bcba
Gautier Borot <g.borot@gmail.com>
2020-02-20 10:09:02 +0100

    how we can render events and store them temporary, and show to every single user the updates

--- src/index.js
+++ src/index.js
@@ -13,8 +13,18 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+let count = 0;
+io.on("connection", socket => {
   console.log("New WebSocket connection");
+
+  socket.emit("countUpdated", count);
+  socket.on("increment", () => {
+    count++;
+    // the first one emits but other users can't see the update until refresh
+    // socket.emit("countUpdated", count);
+    // this one emits to every single connection
+    io.emit("countUpdated", count);
+  });
 });
 
 server.listen(port, () => {
